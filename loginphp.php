a49880c069c34aebfa0284c8b7154a93ddd88698
wsx9721 <924708077@qq.com>
2018-08-20 17:31:50 +0800

    login and register

---
+++ loginphp.php
@@ -0,0 +1,50 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=blog",$username,$password);
+} catch (PDOException $e) {
+    $data="数据库连接失败".$e->getMessage();
+    header(http_response_code(500));
+    die(json_encode($data));
+}
+
+if (isset($_POST['name'])&&isset($_POST['psw'])) {
+    session_start();
+
+    $name=$_POST['name'];
+    $psw=$_POST['psw'];
+
+    if ($name == "" || $psw == "") {
+        $data="请确认信息完整性";
+        header(http_response_code(401));
+        die(json_encode($data));
+    }
+    else {
+        $sql = "SELECT * FROM USER WHERE name = :name";
+        $res=$pdo->prepare($sql);
+        $res->bindValue(':name',$name);
+        $res->execute();
+        $result=$res->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            if ($psw==$result['pwd']) {
+                $data="登录成功";
+                $_SESSION['name']=$name;
+                echo json_encode($data);
+            }
+            else {
+                $data="密码错误";
+                header(http_response_code(422));
+                die(json_encode($data));
+            }
+        }
+        else {
+            $data="用户名不存在";
+            header(http_response_code(422));
+            die(json_encode($data));
+        }
+
+    }
+}
+ ?>
